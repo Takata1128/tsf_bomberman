7df145cc18db1ddcde566865641c8291ee8aaf50
Takata1128 <backkun1128@gmail.com>
2018-12-14 14:10:02 +0900

    ゲームループ作成

--- MainPanel.java
+++ MainPanel.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.KeyListener;
 
-class MainPanel extends JPanel{
+class MainPanel extends JPanel implements Runnable{
 //パネルサイズ
 	private static final int WIDTH = 480;
 	private static final int HEIGHT = 480;
@@ -10,13 +10,16 @@ class MainPanel extends JPanel{
 	private Map map;
 	private Player p1;
 	private KeyController controller;
+	private Thread gameLoop;
 
     public MainPanel(){
     	//パネルの推奨サイズを設定
     	setPreferredSize(new Dimension(WIDTH,HEIGHT));
-			map = new Map(this);
-			controller = new KeyController();
-			p1 = new Player(1,1,"image/Bomberman.png",map,this,controller);
+			map = new Map(this);//マップ生成
+			controller = new KeyController();//キーコントローラー生成
+			p1 = new Player(1,1,"image/Bomberman.png",map,this,controller);//プレイヤー生成
+			gameLoop = new Thread(this);
+			gameLoop.start();
 			setFocusable(true);
 			addKeyListener(controller);
 	  }
@@ -25,7 +28,18 @@ class MainPanel extends JPanel{
     	super.paintComponent(g);
     	//マップを描く
     	map.draw(g);
-			//プレイヤーを描く
-			p1.draw(g);
+		//プレイヤーを描く
+		p1.draw(g);
+    }
+    
+    public void run(){
+    	while(true){
+    		repaint();
+    		try{
+    			Thread.sleep(100);
+    		}catch(InterruptedException e){
+    			e.printStackTrace();
+    		}
+    	}
     }
 }
